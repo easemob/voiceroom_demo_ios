647783874a4510f0440bf6aac4b1176a186b56fc
朱继超 <984065974@qq.com>
2024-08-02 11:21:33 +0800

    Update ResourceBundleExtension.swift

--- EasemobVoice_iOS/EasemobVoice_iOS/Common/Extensions/ResourceBundleExtension.swift
+++ EasemobVoice_iOS/EasemobVoice_iOS/Common/Extensions/ResourceBundleExtension.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-public let resourceBundle = Bundle(path: Bundle.main.path(forResource: "VoiceRoomBaseUIKit", ofType: "bundle") ?? "") ?? Bundle.main
+public let voiceBundle = Bundle(path: Bundle.main.path(forResource: "VoiceRoomBaseUIKit", ofType: "bundle") ?? "") ?? Bundle.main
 
 public extension Bundle {
     
-    static var voiceRoomBundle: Bundle { resourceBundle }
+    static var voiceRoomBundle: Bundle { voiceBundle }
     
 }
